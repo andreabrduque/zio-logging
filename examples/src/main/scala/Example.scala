d229f1ce6a568488051ec81c6b7955004d79bd2a
andreaduque <ad@hypefactors.com>
2022-03-09 10:30:05 +0100

    add other stackdriver fields

--- examples/src/main/scala/Example.scala
+++ examples/src/main/scala/Example.scala
@@ -1,14 +1,16 @@
-import zio.{Cause, ExitCode, FiberId, LogLevel, LogSpan, RuntimeConfigAspect, ZFiberRef, ZLogger, ZTraceElement}
-import zio.logging.{LogAnnotation, LogColor, LogContext, LogFormat, console, json, logContext}
-import zio.logging.LogFormat.{annotation, fiberId, label, level, line, quoted, timestamp}
+import zio.{ Cause, ExitCode, FiberId, LogLevel, LogSpan, RuntimeConfigAspect, ZFiberRef, ZLogger, ZTraceElement }
+import zio.logging.{ LogAnnotation, LogColor, LogContext, LogFormat, console, json, logContext }
+import zio.logging.LogFormat.{ annotation, fiberId, label, level, line, quoted, text, timestamp }
 
-import java.{util => ju}
+import java.{ util => ju }
 import zio.json._
 import zio.json.internal._
 import zio.json.ast._
 import zio.logging.internal.LogAppender
 import zio._
 
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import scala.collection.mutable.ListBuffer
 
 object Example {
@@ -17,12 +19,12 @@ object Example {
    * A [[LogAppender]] for unstructured logging, which simply turns everything
    * into text, and passes it to the given text appender function.
    */
-  def structured(jsonAppender: String => Any): LogAppender = new LogAppender  { self =>
-   override def appendCause(cause: Cause[Any]): Unit = appendText(cause.prettyPrint)
+  def structured(jsonAppender: String => Any): LogAppender = new LogAppender { self =>
+    override def appendCause(cause: Cause[Any]): Unit = appendText(cause.prettyPrint)
 
     override def appendNumeric[A](numeric: A): Unit = appendText(numeric.toString)
 
-    override def appendText(text: String): Unit =  { jsonAppender(text); () }
+    override def appendText(text: String): Unit = { jsonAppender(text); () }
 
     override def closeKeyOpenValue(): Unit = appendText(":")
 
@@ -42,81 +44,121 @@ object Example {
       finally builder.closeValue()
     }
 
+  //custom formatters
+  def timestamp(): LogFormat =
+    text {
+      val now = ZonedDateTime.now()
+      now.toInstant.toEpochMilli.toString
+    }
+
+  val enclosingClass: LogFormat =
+    LogFormat.make { (builder, trace, _, _, _, _, _, _, _) =>
+      trace match {
+        case ZTraceElement(_, file, _) => builder.appendText(s"\"${file}\"")
+        case _                         => builder.appendText("not-available")
+      }
+    }
+
+  val fiberId: LogFormat =
+    LogFormat.make { (builder, _, fiberId, _, _, _, _, _, _) =>
+      builder.appendText(s"\"${fiberId.threadName}\"")
+    }
+
+  val level =  LogFormat.make { (builder, _, _, level, _, _, _, _, _) =>
+    builder.appendText(s"\"${level.label}\"")
+  }
+
+
   def getLogger(): RuntimeConfigAspect = {
-    val builder = new StringBuilder()
+    val builder                   = new StringBuilder()
     val effectful: String => Unit = (x: String) => builder.append(x)
-    val appender: LogAppender = structured(effectful(_))
+    val appender: LogAppender     = structured(effectful(_))
+
+    //make logformatters: comma, openBrace, closeBrace
 
     val custom: LogFormat =
       LogFormat.make { (builder, _, _, _, _, _, _, _, _) =>
         builder.appendText("{")
-      } +  annotation("\"context\"") + LogFormat.make { (builder, _, _, _, _, _, _, _, _) =>
+      } + annotation("\"context\"") + LogFormat.make { (builder, _, _, _, _, _, _, _, _) =>
         builder.appendText(",")
-      } + label("\"message\"", LogFormat.make { (builder, _, _, _, line, _, _, _, _) =>
-        builder.appendText(line())
-      } ) + LogFormat.make { (builder, _, _, _, _, _, _, _, _) =>
+      } + label(
+        "\"message\"",
+        LogFormat.make { (builder, _, _, _, line, _, _, _, _) =>
+          builder.appendText(line())
+        }
+      ) + text(",") + label("\"timestamp\"", timestamp()) + text(",") + label(
+        "\"severity\"",
+        level
+      ) + text(",") + label(
+        "\"thread\"",
+        fiberId
+      ) + text(",") + label(
+        "\"logger\"",
+        enclosingClass
+      ) + LogFormat.make { (builder, _, _, _, _, _, _, _, _) =>
         builder.appendText("}")
       } + LogFormat.newLine
 
-
-
-    val meh = json(appender, (_: Any) => {
-      val result = builder.toString()
-      builder.clear()
-      result
-    }, custom)
+    val meh = json(
+      appender,
+      (_: Any) => {
+        val result = builder.toString()
+        builder.clear()
+        result
+      },
+      custom
+    )
 
     meh
   }
 
 }
 
-final case class LoggingAnnotation[A](key: String, value: A)(implicit encoder: JsonEncoder[A]){
+final case class LoggingAnnotation[A](key: String, value: A)(implicit encoder: JsonEncoder[A]) {
   def encode() = s"{\"${key}\": \"${value.toJson}\"}"
 }
-
 
 sealed trait Logging {
 
   private def escape(value: String) = "\"" + value + "\""
 
-   private def annotateEffect(value: String): ZIO.LogAnnotate =  ZIO.logAnnotate(escape("context"), value)
+  private def annotateEffect(value: String): ZIO.LogAnnotate = ZIO.logAnnotate(escape("context"), value)
 
-  def error[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit] = {
+  def error[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit] =
     annotateEffect(context.encode()) {
       ZIO.logError(escape(message))
     }
-  }
-  def info[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit] = {
+  def info[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit]  =
     annotateEffect(context.encode()) {
-      ZIO.logError(escape(message))
+      ZIO.logInfo(escape(message))
     }
-  }
-  def warn[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit] = {
+  def warn[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit]  =
     annotateEffect(context.encode()) {
-      ZIO.logError(escape(message))
+      ZIO.logWarning(escape(message))
     }
-  }
-  def debug[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit] = {
+  def debug[A](context: LoggingAnnotation[A], message: String)(implicit encoder: JsonEncoder[A]): UIO[Unit] =
     annotateEffect(context.encode()) {
-      ZIO.logError(escape(message))
+      ZIO.logDebug(escape(message))
     }
-  }
 
   //function make or build
 }
 
 object App extends ZIOAppDefault {
 
+  //timestamp
+  //severity
+  //thread
+  //logger
+
   object LoggingLive extends Logging
 
-
- import zio.logging._
+  import zio.logging._
 
   override def hook: RuntimeConfigAspect = Example.getLogger()
 
-    import zio._
-
-  override def run = LoggingLive.error(LoggingAnnotation("size", 10), "the payload is too big")
+  import zio._
+  //test LoggingAnnotation with other objects
+  override def run = LoggingLive.warn(LoggingAnnotation("size", 10), "the payload is too big")
 
 }
